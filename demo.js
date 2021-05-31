7120d55a18ede4f9c8bb6e7a400fe1f6c8a1f394
GITUSER <hudson@inboundnow.com>
2021-05-30 22:12:07 -0500

    Updating demo with better docs

--- demo.js
+++ demo.js
@@ -1,3 +1,10 @@
+/**
+ * Dependencies:
+ *    - jQuery: https://code.jquery.com/jquery-3.6.0.slim.min.js
+ *    - jQuery Validate: https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.2/jquery.validate.min.js
+ *    - jquery.form-slider.js https://raw.githack.com/atwellpub/jQuery-Form-Slider/master/jquery.form-slider.js
+ */
+
 jQuery(document).ready(function() {
     formSlider.init('#example-with-groups');
 });
